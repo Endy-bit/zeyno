8ca749be89b1b3867ec4bbdcf28d5c66deacafc3
Endy-bit <endy81762@gmail.com>
2026-04-12 17:10:04 +0200

    Update info-staff.js

--- plugins/info/info-staff.js
+++ plugins/info/info-staff.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
 
-    // Newsletter globale ChatUnity
+    // Newsletter globale Zeyno
     const cuContext = {
         isForwarded: true,
         forwardingScore: 999,
@@ -13,30 +13,30 @@ let handler = async (m, { conn }) => {
 
     // Schede di contatto (vCard)
     const vcards = [
-        { vcard: `BEGIN:VCARD\nVERSION:5.0\nN:;Endy | CEO;;;\nFN:Endy | CEO\nORG:Zeyno\nTITLE:CEO\nitem1.TEL;waid=393773842461:+39 350 198 9497\nitem1.X-ABLabel:Cellulare\nEND:VCARD` },
-        { vcard: `BEGIN:VCARD\nVERSION:5.0\nN:;medalis;;;\nFN:Diego\nORG:Zeyno\nTITLE:Staff\nitem1.TEL;waid=393520583119:+212 693-877842 3119\nitem1.X-ABLabel:Cellulare\nEND:VCARD` },
-        { vcard: `BEGIN:VCARD\nVERSION:5.0\nN:;ksav;;;\nFN:Matte\nORG:Zeyno\nTITLE:Staff\nitem1.TEL;waid=66621409462:+44 7393 088288 9462\nitem1.X-ABLabel:Cellulare\nEND:VCARD` },
-        { vcard: `BEGIN:VCARD\nVERSION:5.0\nN:;Giuse;;;\nFN:Giuse\nORG:𝐂𝐡𝐚𝐭𝐔𝐧𝐢𝐭𝐲\nTITLE:Staff\nitem1.TEL;waid=393291944932:+39 329 194 4932\nitem1.X-ABLabel:Cellulare\nEND:VCARD` }
+        { vcard: `BEGIN:VCARD\nVERSION:5.0\nN:;Endy | CEO;;;\nFN:Endy | CEO\nORG:Zeyno\nTITLE:CEO\nitem1.TEL;waid=393501989497:+39 350 198 9497\nitem1.X-ABLabel:Cellulare\nEND:VCARD` },
+        { vcard: `BEGIN:VCARD\nVERSION:5.0\nN:;medalis;;;\nFN:Diego\nORG:Zeyno\nTITLE:Staff\nitem1.TEL;waid=212693877842:+212 693-877842 3119\nitem1.X-ABLabel:Cellulare\nEND:VCARD` },
+        { vcard: `BEGIN:VCARD\nVERSION:5.0\nN:;ksav;;;\nFN:Matte\nORG:Zeyno\nTITLE:Staff\nitem1.TEL;waid=447393088288:+44 7393 088288 9462\nitem1.X-ABLabel:Cellulare\nEND:VCARD` },
+        { vcard: `BEGIN:VCARD\nVERSION:5.0\nN:;Exorcism;;;\nFN:Giuse\nORG:Zeyno\nTITLE:Staff\nitem1.TEL;waid=447935218835:+44 7935 218835 4932\nitem1.X-ABLabel:Cellulare\nEND:VCARD` }
     ];
 
     // Testo elegante con i numeri in chiaro
     let testo = `
-୧・︶ ✦ 𝐂𝐡𝐚𝐭𝐔𝐧𝐢𝐭𝐲-𝐁𝐨𝐭 ✦ ︶・୨
+୧・︶ ✦ Chatunity✦ ︶・୨
 ꒷꒦ ‧₊ 🛡️ 𝐒 𝐓 𝐀 𝐅 𝐅 🛡️ ₊‧ ꒷꒦
 ୧・︶ : ︶ : ︶ : ︶ : ︶ : ︶・୨
 
-✦ 👑 +39 377 384 2461 ~ Vale |CEO|
-✦ 👨‍💻 +39 352 058 3119 ~ Diego
-✦ 👨‍💻 +66 62 140 9462 ~ Matte
-✦ 👨‍💻 +39 329 194 4932 ~ Giuse
+✦ 👑 +39 350 198 9497 ~ Endy |CEO|
+✦ 👨‍💻 +212 693-877842 ~ Medalis
+✦ 👨‍💻 +44 7393 088288 ~ ksav
+✦ 👨‍💻 +44 7935 218835 ~ Exorcism
 
-👑 _Il team dietro 𝐂𝐡𝐚𝐭𝐔𝐧𝐢𝐭𝐲._
+👑 _Il team dietro Zeyno._
 ୧・︶ : ︶ ꒷꒦ ‧₊ ୧`.trim();
 
     // 1. Invia le schede contatto (rubrica)
     await conn.sendMessage(m.chat, {
         contacts: {
-            displayName: 'Staff 𝐂𝐡𝐚𝐭𝐔𝐧𝐢𝐭𝐲',
+            displayName: 'Staff Zeyno',
             contacts: vcards
         },
         contextInfo: cuContext
