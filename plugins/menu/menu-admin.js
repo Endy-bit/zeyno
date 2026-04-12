aa2206733efce27c065e877a980dec195846906a
Endy-bit <endy81762@gmail.com>
2026-04-12 08:53:41 +0200

    Update menu-admin.js

--- plugins/menu/menu-admin.js
+++ plugins/menu/menu-admin.js
@@ -9,7 +9,7 @@ const handler = async (message, { conn, usedPrefix, command }) => {
     const groupId = message.isGroup ? message.chat : null;
 
     const menuText = generateMenuText(usedPrefix, userId, groupId);
-    const imagePath = path.join(__dirname, '../../media/immagine.jpg');
+    const imagePath = path.join(__dirname, '../../media/imagine.jpg');
     const footerText = global.t('chooseMenu', userId, groupId);
     const mainMenuText = global.t('mainMenuButton', userId, groupId);
     const ownerMenuText = global.t('ownerMenuButton', userId, groupId);
