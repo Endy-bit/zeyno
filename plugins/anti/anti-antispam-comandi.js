db475a2d88fe887e6c0e28f5647e49f0ceeafd58
slyce-120 <psjq55h4fh@privaterelay.appleid.com>
2026-04-30 22:50:54 +0200

    Update anti-antispam-comandi.js

--- plugins/anti/anti-antispam-comandi.js
+++ plugins/anti/anti-antispam-comandi.js
@@ -175,7 +175,7 @@ async function enforceCommandAntispam(conn, m, chat) {
       text: `『 ⚠ 』 Anti-spam comandi\n\n@${getJidUser(m.sender)} hai superato il limite di comandi consecutivi.\nAttendi *${Math.ceil(COMMAND_SPAM_USER_COOLDOWN / 1000)} secondi* prima di riprovare.`,
       mentions: [m.sender]
     })
-    m.commandBlocked = true
+    m.commandBlocked = false
     return true
   }
 
